2d401bb74fbe4a9ae546fea279489eb70788fb31
triple-s-rank <specialforwar@icloud.com>
2022-06-30 20:07:41 +0300

    Setup filtering in admin UI

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = 'created_at',
     list_display = 'town', 'address', 'price', 'new_building', 'construction_year'
     list_editable = 'new_building',
+    list_filter = 'new_building', 'has_balcony',
 
 
 admin.site.register(Flat, FlatAdmin)
